318c040f830484502f8d71b8eee7c75bf92e2576
Addison Hill <adddison.hill@gmail.com>
2020-01-06 00:48:34 -0600

    finished user model and router, can register and login, no middleware yet

--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const authRouter = require("../auth/auth-router");
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Restaurant Passport is LIVE" });
---
+++ auth/auth-router.js
@@ -0,0 +1,67 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("../users/users-model");
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(registered => {
+      res.status(201).json(registered);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Error registering" });
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = signToken(user);
+        res.status(200).json({
+          token,
+          message: `Welcome ${user.username}!`
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Login failed" });
+    });
+});
+
+function signToken(user) {
+  const payload = {
+    username: user.username,
+    id: user.id
+  };
+
+  const secret = "secret krabby patty ingredient";
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+router.get("/users", (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+module.exports = router;
