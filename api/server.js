ed4175dad61d29b097405805efb96f0426f99076
Addison Hill <adddison.hill@gmail.com>
2020-01-07 19:08:11 -0600

    made a route to yelps API, also made env file and put it in gitignore

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const helmet = require("helmet");
 const authRouter = require("../auth/auth-router");
 const passRouter = require("../passport/passport-router");
 const restricted = require("../auth/auth-middleware");
+const yelpRouter = require("./yelp-router");
 
 const server = express();
 
@@ -13,6 +14,7 @@ server.use(express.json());
 
 server.use("/api/auth", authRouter);
 server.use("/api/auth/passport", restricted, passRouter);
+server.use("/api/auth/restaurants", yelpRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Restaurant Passport is LIVE" });
